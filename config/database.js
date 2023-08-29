a219dc3d3f0230bd6daa131c844771c79c51e380
GAMATAM VAJINTHA <gamatam.vajintha@zysk.tech>
2023-08-29 16:47:19 +0530

    files added

--- config/database.js
+++ config/database.js
@@ -8,6 +8,7 @@ module.exports = ({ env }) => ({
         user: env('DATABASE_USERNAME', 'postgres'),
         password: env('DATABASE_PASSWORD', 'Vajintha@1511'),
         schema: env('DATABASE_SCHEMA', 'public'),
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
 });
