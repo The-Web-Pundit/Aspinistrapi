6bf337380ebe5b40ba0db466e117e1c3590c8031
GAMATAM VAJINTHA <gamatam.vajintha@zysk.tech>
2023-08-30 10:51:33 +0530

    files****

--- config/database.js
+++ config/database.js
@@ -1,47 +1,27 @@
-// local database
+module.exports = ({ env }) => ({
 
-// module.exports = ({ env }) => ({
-//   connection:{
-//        client: 'postgres',
-//        connection:{
-//         host: env('DATABASE_HOST'),
-//         port: env.int('DATABASE_PORT'),
-//         database: env('DATABASE_NAME'),
-//         user: env('DATABASE_USERNAME'),
-//         password: env('DATABASE_PASSWORD'),
-//         schema: env('DATABASE_SCHEMA'),
-//         ssl: env.bool('DATABASE_SSL'),
-//       },
-//     },
-// });
+  connection: {
 
+    client: env('DATABASE_CLIENT'),
 
-const path = require('path');
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'postgres');
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: env.bool('DATABASE_SSL', true) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    connection: {
+
+      host: env('DATABASE_HOST'),
+
+      port: env.int('DATABASE_PORT'),
+
+      database: env('DATABASE_NAME'),
+
+      user: env('DATABASE_USERNAME'),
+
+      password: env('DATABASE_PASSWORD'),
+
+      schema: env('DATABASE_SCHEMA'),
+
+      ssl: env.bool('DATABASE_SSL'),
+
     },
-  }
-}
+
+  },
+
+});
